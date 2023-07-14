349dbe4da256317b6c4822207355c51d21951f30
Augusto Zanellato <augusto.zanellato@gmail.com>
2023-07-14 12:28:49 +0200

    Add prometheus exporter predisposition

--- wggen/client.py
+++ wggen/client.py
@@ -7,10 +7,24 @@ from wggen.server import WGServer
 
 
 class WGClient:
-    def __init__(self, server: WGServer, name: str, address: IPv4Address):
+    def __init__(
+        self,
+        server: WGServer,
+        filename: str,
+        address: IPv4Address,
+        friendly_name: str | None = None,
+        friendly_json: dict | None = None,
+    ):
         key = WireguardKey.generate()
         interface = IPv4Interface(address)
-        self.name = name
+        self.name = filename
         self.config = WireguardConfig(private_key=key, addresses=[interface])
         self.config.add_peer(server.peer)
-        server.config.add_peer(WireguardPeer(public_key=key.public_key(), allowed_ips=[interface]))
+        server.config.add_peer(
+            WireguardPeer(
+                public_key=key.public_key(),
+                allowed_ips=[interface],
+                friendly_name=friendly_name,
+                friendly_json=friendly_json,
+            )
+        )
--- wggen/group.py
+++ wggen/group.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from ipaddress import IPv4Address, IPv4Network
+from ipaddress import IPv4Network
 
 from wggen.client import WGClient
 from wggen.server import WGServer
@@ -7,6 +7,7 @@ from wggen.server import WGServer
 
 @dataclass(slots=True)
 class SubGroup:
+    group: str
     name: str
     subnet: IPv4Network
     file_prefix: str
@@ -14,11 +15,30 @@ class SubGroup:
     configs: list[WGClient] = field(init=False)
 
     def generate_configs(self, server: WGServer) -> None:
+        friendly_json = {
+            "group": self.group,
+            "subgroup": self.name,
+        }
         if self.peer_count == 1:
-            self.configs = [WGClient(server, self.file_prefix, self.subnet[1])]
+            self.configs = [
+                WGClient(
+                    server,
+                    self.file_prefix,
+                    self.subnet[1],
+                    self.file_prefix,
+                    friendly_json | {"client": self.file_prefix},
+                )
+            ]
         else:
             self.configs = [
-                WGClient(server, f"{self.file_prefix}{i}", self.subnet[i]) for i in range(1, self.peer_count + 1)
+                WGClient(
+                    server,
+                    f"{self.file_prefix}{i}",
+                    self.subnet[i],
+                    f"{self.name}_{self.file_prefix}{i}",
+                    friendly_json | {"client": f"{self.file_prefix}{i}"},
+                )
+                for i in range(1, self.peer_count + 1)
             ]
 
 
@@ -37,6 +57,7 @@ class Group:
             if self.single_peer_per_subgroup:
                 subgroups = [
                     SubGroup(
+                        self.name,
                         f"{self.name}{i}",
                         subnet,
                         f"{self.file_prefix}{i}" if self.file_prefix else f"{self.name}{i}",
@@ -48,13 +69,21 @@ class Group:
                 ]
             else:
                 subgroups = [
-                    SubGroup(f"{self.name}{i}", subnet, self.file_prefix or f"{self.name}{i}_", self.peers_per_subgroup)
+                    SubGroup(
+                        self.name,
+                        f"{self.name}{i}",
+                        subnet,
+                        self.file_prefix or f"{self.name}{i}_",
+                        self.peers_per_subgroup,
+                    )
                     for i, subnet in zip(
                         range(self.subgroup_count), self.subnet.subnets(new_prefix=self.subgroup_bits), strict=False
                     )
                 ]
         else:
-            subgroups = [SubGroup(self.name, self.subnet, self.file_prefix or self.name, self.peers_per_subgroup)]
+            subgroups = [
+                SubGroup(self.name, self.name, self.subnet, self.file_prefix or self.name, self.peers_per_subgroup)
+            ]
         self.subgroups = subgroups
 
     @property
